30ae47ff5b57a349d7e7eb12445018ee4c65ed93
SysWOW64UwU <81893400+SysWOW64UwU@users.noreply.github.com>
2022-11-04 00:51:29 +0530

    Formatted YT Downloader Plugins

--- ICBot/addons/ytmusicdl.py
+++ ICBot/addons/ytmusicdl.py
@@ -8,190 +8,175 @@ from dotenv import load_dotenv
 import os
 import asyncio
 from concurrent.futures import ThreadPoolExecutor
+
+
 executor = ThreadPoolExecutor(10)
 loop = asyncio.get_event_loop()
-
 load_dotenv()
 '''===========EDITABLES==========='''
 
-preFix = ""  # Change this character for a custom command prefix, leave empty to use the master prefix defined in .env, the fallback prefix is  TG's default "/" prefix
-# Change the command list accordingly
+preFix = ""
 cmds = ["yt", F"yt{os.getenv('BOT_UNAME')}"]
-# Provide brief and meaningful help information about what the command is for
-HELP = F"{preFix if preFix else os.getenv('MASTER_PREFIX', '/')}{cmds[0]} - upload music from yt"
+HELP = F"{preFix if preFix else os.getenv('MASTER_PREFIX', '/')}{cmds[0]} link - upload music from youtube"
 # Youtube video thumbnail link format
 YT_THUMB_LINK = 'https://i.ytimg.com/vi/{id}/mqdefault.jpg'
 CAPTION = '''
 <b>Your Song is downloaded</b> - {tag}
 <b>Title</b> - <code>{title}</code>
-<b>Artist</b> - <code>{uploader}</code>
-'''
+<b>Artist</b> - <code>{uploader}</code>'''
 CAPTION_PLAYLIST = '''
 <b>Title</b> - <code>{title}</code>
-<b>Uploader</b> - <code>{uploader}</code>
-'''
+<b>Uploader</b> - <code>{uploader}</code>'''
+
 '''-------------------------------'''
 
 
 def command(cmd): return filters.command(
-    cmd, prefixes=preFix if preFix else os.getenv('MASTER_PREFIX', '/'))
+  cmd, prefixes=preFix if preFix else os.getenv('MASTER_PREFIX', '/'))
 
 
 def dl_thumbnail_image(link, msg):
-    response = requests.get(link)
-    if response.status_code == 200:
-        try:
-            os.remove(f"thumbnail_{msg.from_user.id}.jpg")
-        except:
-            pass
+  response = requests.get(link)
+  if response.status_code == 200:
+    try:
+      os.remove(f"thumbnail_{msg.from_user.id}.jpg")
+    except:
+      pass
 
-        with open(f"thumbnail_{msg.from_user.id}.jpg", "wb") as cover:
-            cover.write(response.content)
-        return True
-    else:
-        return None
+    with open(f"thumbnail_{msg.from_user.id}.jpg", "wb") as cover:
+      cover.write(response.content)
+    return True
+  else:
+    return None
 
 
 @Client.on_message(command(cmds))
-# The function name will be myFunc if the file name is myFunc.py, not necessarily but try to stick to this convention
 async def ytmusicdl(app, msg):
-    args = msg.text.split(" ")
-    if len(args) == 1:
-        return await msg.reply("<b>Use like this : </b><code>/yt link</code>")
-    elif len(args) == 2:
-        link = args[-1].strip()
-    else:
-        return await msg.reply("<b>Use like this : </b><code>/yt link</code>")
+  args = msg.text.split(" ")
+  if len(args) == 1:
+    return await msg.reply(F"Usage:\n`{HELP}`", quote = 1)
+  elif len(args) == 2:
+    link = args[-1].strip()
+  else:
+    return await msg.reply(F"Usage:\n`{HELP}`", quote = 1)
 
-    ydl_opts = {
-        'format': 'bestaudio/best',
-        'writethumbnail': 'True',
-        'keepvideo': 'False',
-        'postprocessors': [
-            {
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3',
-                'preferredquality': '0',
-            },
-            {
-                'key': 'FFmpegMetadata',
-            },
-            {
-                'key': 'EmbedThumbnail',
-            }
-        ],
-        'outtmpl': f"music_{msg.from_user.id}/%(title)s.%(ext)s"
-    }
+  ydl_opts = {
+    'format': 'bestaudio/best',
+    'writethumbnail': 'True',
+    'keepvideo': 'False',
+    'postprocessors': [
+      {
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '0',
+      },
+      {
+        'key': 'FFmpegMetadata',
+      },
+      {
+        'key': 'EmbedThumbnail',
+      }
+    ],
+    'outtmpl': f"music_{msg.from_user.id}/%(title)s.%(ext)s"
+  }
 
-    song_path = os.path.join(os.getcwd(), f"music_{msg.from_user.id}")
+  song_path = os.path.join(os.getcwd(), f"music_{msg.from_user.id}")
 
-    if msg.from_user.username:
-        tag = f"@{msg.from_user.username}"
-    else:
-        if msg.from_user.first_name == 'ЅуѕＷΟＷ６４':
-            tag = msg.from_user.mention("SystemWoW")
-        else:
-            tag = msg.from_user.mention()
+  tag = F"[{msg.from_user.first_name}](tg://user?id={msg.from_user.id})"
 
-    reply = await msg.reply_animation(
-        animation="https://i.pinimg.com/originals/48/6a/a0/486aa0fa1658b7522ecd8918908ece40.gif",
-        caption=f"<code>Downloading from YT...</code>",
+  reply = await msg.reply_animation(
+    animation="https://i.pinimg.com/originals/48/6a/a0/486aa0fa1658b7522ecd8918908ece40.gif",
+    caption="<code>Downloading from YT...</code>",
+  )
+  try:
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+      info = ydl.extract_info(link)
+  except:
+    shutil.rmtree(song_path)
+    return await reply.edit_media(
+      InputMediaPhoto(
+        media="https://c4.wallpaperflare.com/wallpaper/976/117/318/anime-girls-404-not-found-glowing-eyes-girls-frontline-wallpaper-preview.jpg",
+        caption=f'<b>Link is invalid : </b><code>{link}</code>',
+      )
     )
-    try:
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            info = ydl.extract_info(link)
-    except:
-        shutil.rmtree(song_path)
-        return await reply.edit_media(
-            InputMediaPhoto(
-                media="https://c4.wallpaperflare.com/wallpaper/976/117/318/anime-girls-404-not-found-glowing-eyes-girls-frontline-wallpaper-preview.jpg",
-                caption=f'<b>Link is invalid : </b><code>{link}</code>',
-            )
+  try:
+    await reply.edit_media(
+      InputMediaAnimation(
+        media="https://i.pinimg.com/originals/48/6a/a0/486aa0fa1658b7522ecd8918908ece40.gif",
+        caption="<code>Download completed, Sending to telegram...</code>"
         )
-    try:
-        await reply.edit_media(
-            InputMediaAnimation(
-                media="https://i.pinimg.com/originals/48/6a/a0/486aa0fa1658b7522ecd8918908ece40.gif",
-                caption="<code>Download completed, Sending to telegram...</code>"
-            )
-        )
-    except:
-        pass
+    )
+  except: pass
 
-    if 'playlist' in link.lower():
-        await playlist_upload(app, msg, reply, info, song_path, tag)
-    else:
-        await song_upload(msg, reply, info, song_path, tag)
+  if 'playlist' in link.lower():
+      await playlist_upload(app, msg, reply, info, song_path, tag)
+  else: await song_upload(msg, reply, info, song_path, tag)
 
 
 async def song_upload(msg, reply, info, song_path, tag):
-    thumb_status = await loop.run_in_executor(executor, lambda: dl_thumbnail_image(YT_THUMB_LINK.format(id=info['id']), msg))
-    for song in os.listdir(song_path):
-        fpath = os.path.join(song_path, song)
-        if not song.endswith(".mp3"):
-            os.remove(fpath)
-        else:
-            if thumb_status:
-                await reply.edit_media(
-                    InputMediaAudio(
-                        media=fpath,
-                        thumb=f'thumbnail_{msg.from_user.id}.jpg',
-                        caption=CAPTION.format(
-                            tag=tag,
-                            title=info['title'],
-                            uploader=info['uploader']
-                        )
-                    )
-                )
-            else:
-                await reply.edit_media(
-                    InputMediaAudio(
-                        media=fpath,
-                        caption=CAPTION.format(
-                            tag=tag,
-                            title=info['title'],
-                            uploader=info['uploader']
-                        )
-                    )
-                )
-            os.remove(fpath)
-    try:
-        os.remove(f'thumbnail_{msg.from_user.id}.jpg')
-    except:
-        pass
+  thumb_status = await loop.run_in_executor(executor, lambda: dl_thumbnail_image(YT_THUMB_LINK.format(id=info['id']), msg))
+  for song in os.listdir(song_path):
+    fpath = os.path.join(song_path, song)
+    if not song.endswith(".mp3"):
+      os.remove(fpath)
+    else:
+      if thumb_status:
+        await reply.edit_media(
+          InputMediaAudio(
+            media=fpath,
+            thumb=f'thumbnail_{msg.from_user.id}.jpg',
+            caption=CAPTION.format(
+              tag=tag,
+              title=info['title'],
+              uploader=info['uploader']
+            )
+          )
+        )
+      else:
+        await reply.edit_media(
+          InputMediaAudio(
+            media=fpath,
+            caption=CAPTION.format(
+              tag=tag,
+              title=info['title'],
+              uploader=info['uploader']
+            )
+          )
+        )
+      os.remove(fpath)
+  try: os.remove(f'thumbnail_{msg.from_user.id}.jpg')
+  except: pass
 
 
 async def playlist_upload(client, msg, reply, info, song_path, tag):
-    for song in info["entries"]:
-        thumb_status = await loop.run_in_executor(executor, lambda: dl_thumbnail_image(YT_THUMB_LINK.format(id=song['id']), msg))
-        fpath = os.path.join(song_path, f'{song["title"]}.mp3')
-        if thumb_status:
-            await client.send_audio(
-                chat_id=msg.chat.id,
-                audio=fpath,
-                thumb=f'thumbnail_{msg.from_user.id}.jpg',
-                caption=CAPTION_PLAYLIST.format(
-                    title=song['title'],
-                    uploader=song['uploader']
-                )
-            )
-        else:
-            await client.send_audio(
-                chat_id=msg.chat.id,
-                audio=fpath,
-                caption=CAPTION_PLAYLIST.format(
-                    title=song['title'],
-                    uploader=song['uploader']
-                )
-            )
-    shutil.rmtree(song_path)
-    reply.edit_media(
-        InputMediaPhoto(
-            media=f"thumbnail_{msg.from_user.id}.jpg",
-            caption=f"<b>Your Playlist has been Uploaded</b> - {tag}"
+  for song in info["entries"]:
+    thumb_status = await loop.run_in_executor(executor, lambda: dl_thumbnail_image(YT_THUMB_LINK.format(id=song['id']), msg))
+    fpath = os.path.join(song_path, f'{song["title"]}.mp3')
+    if thumb_status:
+      await client.send_audio(
+        chat_id=msg.chat.id,
+        audio=fpath,
+        thumb=f'thumbnail_{msg.from_user.id}.jpg',
+        caption=CAPTION_PLAYLIST.format(
+          title=song['title'],
+          uploader=song['uploader']
         )
+      )
+    else:
+      await client.send_audio(
+        chat_id=msg.chat.id,
+        audio=fpath,
+        caption=CAPTION_PLAYLIST.format(
+          title=song['title'],
+          uploader=song['uploader']
+        )
+      )
+  shutil.rmtree(song_path)
+  reply.edit_media(
+    InputMediaPhoto(
+      media=f"thumbnail_{msg.from_user.id}.jpg",
+      caption=f"<b>Your Playlist has been Uploaded</b> - {tag}"
     )
-    try:
-        os.remove(f'thumbnail_{msg.from_user.id}.jpg')
-    except:
-        pass
+  )
+  try: os.remove(f'thumbnail_{msg.from_user.id}.jpg')
+  except: pass
--- ICBot/addons/ytmusichannel.py
+++ ICBot/addons/ytmusichannel.py
@@ -8,17 +8,16 @@ from dotenv import load_dotenv
 import os
 import asyncio
 from concurrent.futures import ThreadPoolExecutor
+
+
 executor = ThreadPoolExecutor(10)
 loop = asyncio.get_event_loop()
-
 load_dotenv()
 '''===========EDITABLES==========='''
 
-preFix = ""  # Change this character for a custom command prefix, leave empty to use the master prefix defined in .env, the fallback prefix is  TG's default "/" prefix
-# Change the command list accordingly
+preFix = ""
 cmds = ["ytch", F"ytch{os.getenv('BOT_UNAME')}"]
-# Provide brief and meaningful help information about what the command is for
-HELP = F"{preFix if preFix else os.getenv('MASTER_PREFIX', '/')}{cmds[0]} - upload music from yt"
+HELP = F"{preFix if preFix else os.getenv('MASTER_PREFIX', '/')}{cmds[0]} link - upload music from youtube to the specified channel"
 # Youtube video thumbnail link format
 YT_THUMB_LINK = 'https://i.ytimg.com/vi/{id}/mqdefault.jpg'
 MUSIC_CHANNEL = -1001746607360
@@ -26,183 +25,173 @@ CAPTION = '''
 <b>Title</b> - <code>{title}</code>
 <b>Artist</b> - <code>{uploader}</code>
 '''
+
 '''-------------------------------'''
 
 
 def command(cmd): return filters.command(
-    cmd, prefixes=preFix if preFix else os.getenv('MASTER_PREFIX', '/'))
+  cmd, prefixes=preFix if preFix else os.getenv('MASTER_PREFIX', '/'))
 
 
 def dl_thumbnail_image(link, msg):
-    response = requests.get(link)
-    if response.status_code == 200:
-        try:
-            os.remove(f"thumbnail_{msg.from_user.id}.jpg")
-        except:
-            pass
+  response = requests.get(link)
+  if response.status_code == 200:
+    try:
+      os.remove(f"thumbnail_{msg.from_user.id}.jpg")
+    except: pass
 
-        with open(f"thumbnail_{msg.from_user.id}.jpg", "wb") as cover:
-            cover.write(response.content)
-        return True
-    else:
-        return None
+    with open(f"thumbnail_{msg.from_user.id}.jpg", "wb") as cover:
+      cover.write(response.content)
+    return True
+  else:
+    return None
 
 
 @Client.on_message(command(cmds))
-# The function name will be myFunc if the file name is myFunc.py, not necessarily but try to stick to this convention
 async def ytmusichannel(app, msg):
-    args = msg.text.split(" ")
-    if len(args) == 1:
-        return await msg.reply("<b>Use like this : </b><code>/yt link</code>")
-    elif len(args) == 2:
-        link = args[-1].strip()
-    else:
-        return await msg.reply("<b>Use like this : </b><code>/yt link</code>")
+  args = msg.text.split(" ")
+  if len(args) == 1:
+    return await msg.reply(F"Usage:\n`{HELP}`", quote = 1)
+  elif len(args) == 2:
+    link = args[-1].strip()
+  else:
+    return await msg.reply(F"Usage:\n`{HELP}`", quote = 1)
 
-    ydl_opts = {
-        'format': 'bestaudio/best',
-        'writethumbnail': 'True',
-        'keepvideo': 'False',
-        'postprocessors': [
-            {
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3',
-                'preferredquality': '0',
-            },
-            {
-                'key': 'FFmpegMetadata',
-            },
-            {
-                'key': 'EmbedThumbnail',
-            }
-        ],
-        'outtmpl': f"music_{msg.from_user.id}/%(title)s.%(ext)s"
-    }
+  ydl_opts = {
+    'format': 'bestaudio/best',
+    'writethumbnail': 'True',
+    'keepvideo': 'False',
+    'postprocessors': [
+      {
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '0',
+      },
+      {
+        'key': 'FFmpegMetadata',
+      },
+      {
+        'key': 'EmbedThumbnail',
+      }
+    ],
+    'outtmpl': f"music_{msg.from_user.id}/%(title)s.%(ext)s"
+  }
 
-    song_path = os.path.join(os.getcwd(), f"music_{msg.from_user.id}")
+  song_path = os.path.join(os.getcwd(), f"music_{msg.from_user.id}")
 
-    if msg.from_user.username:
-        tag = f"@{msg.from_user.username}"
-    else:
-        if msg.from_user.first_name == 'ЅуѕＷΟＷ６４':
-            tag = msg.from_user.mention("SystemWoW")
-        else:
-            tag = msg.from_user.mention()
+  tag = F"[{msg.from_user.first_name}](tg://user?id={msg.from_user.id})"
 
-    reply = await msg.reply_animation(
-        animation="https://i.pinimg.com/originals/48/6a/a0/486aa0fa1658b7522ecd8918908ece40.gif",
-        caption=f"<code>Downloading from YT...</code>",
+  reply = await msg.reply_animation(
+    animation="https://i.pinimg.com/originals/48/6a/a0/486aa0fa1658b7522ecd8918908ece40.gif",
+    caption=f"<code>Downloading from YT...</code>",
+  )
+  try:
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+      info = ydl.extract_info(link)
+  except:
+    shutil.rmtree(song_path)
+    return await reply.edit_media(
+      InputMediaPhoto(
+        media="https://c4.wallpaperflare.com/wallpaper/976/117/318/anime-girls-404-not-found-glowing-eyes-girls-frontline-wallpaper-preview.jpg",
+        caption=f'<b>Link is invalid : </b><code>{link}</code>',
+      )
     )
-    try:
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            info = ydl.extract_info(link)
-    except:
-        shutil.rmtree(song_path)
-        return await reply.edit_media(
-            InputMediaPhoto(
-                media="https://c4.wallpaperflare.com/wallpaper/976/117/318/anime-girls-404-not-found-glowing-eyes-girls-frontline-wallpaper-preview.jpg",
-                caption=f'<b>Link is invalid : </b><code>{link}</code>',
-            )
-        )
-    try:
-        await reply.edit_media(
-            InputMediaAnimation(
-                media="https://i.pinimg.com/originals/48/6a/a0/486aa0fa1658b7522ecd8918908ece40.gif",
-                caption="<code>Download completed, Sending to telegram...</code>"
-            )
-        )
-    except:
-        pass
+  try:
+    await reply.edit_media(
+      InputMediaAnimation(
+        media="https://i.pinimg.com/originals/48/6a/a0/486aa0fa1658b7522ecd8918908ece40.gif",
+        caption="<code>Download completed, Sending to telegram...</code>"
+      )
+    )
+  except: pass
 
-    if 'playlist' in link.lower():
-        await playlist_upload(app, msg, reply, info, song_path, tag)
-    else:
-        await song_upload(app, msg, reply, info, song_path, tag)
+  if 'playlist' in link.lower():
+    await playlist_upload(app, msg, reply, info, song_path, tag)
+  else:
+    await song_upload(app, msg, reply, info, song_path, tag)
 
 
 async def song_upload(client, msg, reply, info, song_path, tag):
-    thumb_status = await loop.run_in_executor(executor, lambda: dl_thumbnail_image(YT_THUMB_LINK.format(id=info['id']), msg))
-    for song in os.listdir(song_path):
-        fpath = os.path.join(song_path, song)
-        if not song.endswith(".mp3"):
-            os.remove(fpath)
-        else:
-            if thumb_status:
-                song_uploaded = await client.send_audio(
-                    chat_id=MUSIC_CHANNEL,
-                    audio=fpath,
-                    thumb=f'thumbnail_{msg.from_user.id}.jpg',
-                    caption=CAPTION.format(
-                        title=info['title'],
-                        uploader=info['uploader']
-                    ))
+  thumb_status = await loop.run_in_executor(executor, lambda: dl_thumbnail_image(YT_THUMB_LINK.format(id=info['id']), msg))
+  for song in os.listdir(song_path):
+    fpath = os.path.join(song_path, song)
+    if not song.endswith(".mp3"):
+      os.remove(fpath)
+    else:
+      if thumb_status:
+        song_uploaded = await client.send_audio(
+          chat_id=MUSIC_CHANNEL,
+          audio=fpath,
+          thumb=f'thumbnail_{msg.from_user.id}.jpg',
+          caption=CAPTION.format(
+            title=info['title'],
+            uploader=info['uploader']
+          )
+        )
 
-            else:
-                song_uploaded = await client.send_audio(
-                    chat_id=MUSIC_CHANNEL,
-                    audio=fpath,
-                    caption=CAPTION.format(
-                        title=info['title'],
-                        uploader=info['uploader']
-                    ))
-            os.remove(fpath)
+      else:
+        song_uploaded = await client.send_audio(
+          chat_id=MUSIC_CHANNEL,
+          audio=fpath,
+          caption=CAPTION.format(
+              title=info['title'],
+              uploader=info['uploader']
+          )
+        )
+      os.remove(fpath)
 
-    buttons = [InlineKeyboardButton(
-        text="Access Here",
-        url=f'https://t.me/comding_music/{song_uploaded.id}'
-    )]
-    markup = InlineKeyboardMarkup([buttons])
-    await reply.edit_media(
-        media=InputMediaPhoto(
-            media=f"thumbnail_{msg.from_user.id}.jpg",
-            caption=f"<b>Your Song has been Uploaded -</b> {tag}"
-        ),
-        reply_markup=markup
-    )
-    try:
-        os.remove(f'thumbnail_{msg.from_user.id}.jpg')
-    except:
-        pass
+  buttons = [InlineKeyboardButton(
+    text="Access Here",
+    url=f'https://t.me/comding_music/{song_uploaded.id}'
+  )]
+  markup = InlineKeyboardMarkup([buttons])
+  await reply.edit_media(
+    media=InputMediaPhoto(
+      media=f"thumbnail_{msg.from_user.id}.jpg",
+      caption=f"<b>Your Song has been Uploaded -</b> {tag}"
+    ),
+    reply_markup=markup
+  )
+  try: os.remove(f'thumbnail_{msg.from_user.id}.jpg')
+  except: pass
 
 
 async def playlist_upload(client, msg, reply, info, song_path, tag):
-    for song in info["entries"]:
-        thumb_status = await loop.run_in_executor(executor, lambda: dl_thumbnail_image(YT_THUMB_LINK.format(id=song['id']), msg))
-        fpath = os.path.join(song_path, f'{song["title"]}.mp3')
-        if thumb_status:
-            song_uploaded = await client.send_audio(
-                chat_id=MUSIC_CHANNEL,
-                audio=fpath,
-                thumb=f'thumbnail_{msg.from_user.id}.jpg',
-                caption=CAPTION.format(
-                    title=song['title'],
-                    uploader=song['uploader']
-                )
-            )
-        else:
-            song_uploaded = await client.send_audio(
-                chat_id=msg.chat.id,
-                audio=fpath,
-                caption=CAPTION.format(
-                    title=song['title'],
-                    uploader=song['uploader']
-                )
-            )
+  for song in info["entries"]:
+    thumb_status = await loop.run_in_executor(executor, lambda: dl_thumbnail_image(YT_THUMB_LINK.format(id=song['id']), msg))
+    fpath = os.path.join(song_path, f'{song["title"]}.mp3')
+    if thumb_status:
+      song_uploaded = await client.send_audio(
+        chat_id=MUSIC_CHANNEL,
+        audio=fpath,
+        thumb=f'thumbnail_{msg.from_user.id}.jpg',
+        caption=CAPTION.format(
+          title=song['title'],
+          uploader=song['uploader']
+        )
+      )
+    else:
+      song_uploaded = await client.send_audio(
+        chat_id=msg.chat.id,
+        audio=fpath,
+        caption=CAPTION.format(
+          title=song['title'],
+          uploader=song['uploader']
+          )
+      )
 
-    buttons = [InlineKeyboardButton(
-        text="Access Here",
-        url=f'https://t.me/comding_music/{song_uploaded.id}'
-    )]
-    markup = InlineKeyboardMarkup([buttons])
-    shutil.rmtree(song_path)
-    await reply.edit_media(
-        media=InputMediaPhoto(
-            media=f"thumbnail_{msg.from_user.id}.jpg",
-            caption=f"<b>Your Playlist has been Uploaded</b> - {tag}"
-        ),
-        reply_markup=markup
-    )
-    try:
-        os.remove(f'thumbnail_{msg.from_user.id}.jpg')
-    except:
-        pass
+  buttons = [InlineKeyboardButton(
+    text="Access Here",
+    url=f'https://t.me/comding_music/{song_uploaded.id}'
+  )]
+  markup = InlineKeyboardMarkup([buttons])
+  shutil.rmtree(song_path)
+  await reply.edit_media(
+    media=InputMediaPhoto(
+        media=f"thumbnail_{msg.from_user.id}.jpg",
+        caption=f"<b>Your Playlist has been Uploaded</b> - {tag}"
+    ),
+    reply_markup=markup
+  )
+  try: os.remove(f'thumbnail_{msg.from_user.id}.jpg')
+  except: pass
