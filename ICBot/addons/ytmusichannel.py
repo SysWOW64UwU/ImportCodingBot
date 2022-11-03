db734e4f9e0fdaee513cf4e0ac3c4bc2f2446a61
Mohd Zaid <bzatch70@gmail.com>
2022-11-03 18:08:57 +0530

    Minor bu\g fix

--- ICBot/addons/ytmusichannel.py
+++ ICBot/addons/ytmusichannel.py
@@ -94,6 +94,7 @@ async def ytmusichannel(app, msg):
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(link)
     except:
+        shutil.rmtree(song_path)
         return await reply.edit_media(
             InputMediaPhoto(
                 media="https://c4.wallpaperflare.com/wallpaper/976/117/318/anime-girls-404-not-found-glowing-eyes-girls-frontline-wallpaper-preview.jpg",
